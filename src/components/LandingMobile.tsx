fd17a57a12491a67cde50aa8a9d01eab8ac7b2b4
Samantha Gundotra <40836349+samgundotra@users.noreply.github.com>
2022-07-27 16:36:27 -0700

    mobile about me section

--- src/components/LandingMobile.tsx
+++ src/components/LandingMobile.tsx
@@ -8,6 +8,7 @@ import PersonalGlassCard from "./PersonalGlassCard";
 
 function LandingMobile(props: {img: string}) {
     return (
+      <div className="gradient-container">
         <section className='mobile-landing-section'>
             {/* <header className='glass-personal-header'>
                     <h2 className='glass-h2'>Hi, my name is</h2>
@@ -45,7 +46,37 @@ function LandingMobile(props: {img: string}) {
                 <img src='https://emoji.craftwork.design/images/modal/thought-balloon.png'></img>
               </div> */}
           </div>
+          
+          <div id="about-me-section" className='glass-card-container'>
+                <div className='mobile-glass-box-personal'>
+                  
+                <div className='mobile-personal-pic'>
+                  <img src="https://media-exp1.licdn.com/dms/image/C4E03AQG34L54kPzS4A/profile-displayphoto-shrink_400_400/0/1597249710921?e=1664409600&v=beta&t=ps32BQ-uQPMwZpokGsN8vldmIDRyGJESfnfZmx7Bftg"></img>
+                </div>
+                
+                <div className='mobile-about-box-content'>
+                  <h2 className="glass-h2">A little bit about me... 👋🏽</h2>
+                  <p>I am a California native navigating my way through Rhode Island winters 🌱 </p>
+                  <p>I am the proud sister of a Solana dev 🛠️<a href="https://twitter.com/ngundotra"> @ngundotra</a> </p>
+                  <p>I built this website from scratch using React JS 👩🏽‍💻 Inspiration comes from <a href="https://www.pinterest.com/samgundotra/ui-inspo/">this pinterest board.</a> </p>
+                  <ul>Things I don't get tired of:
+                    <li>Collaging</li>
+                    <li>Pho</li>
+                    <li>Depop</li>
+                    <li>Creating Notion templates</li>
+                    <li>Selling Sunset on Netflix</li>
+                    <li>Making pinterest boards that I probably forget about</li>
+                  </ul>
+                </div>
+            </div>
+          </div>
+
+
+
+
         </section>
+
+      </div>
 
 
 
